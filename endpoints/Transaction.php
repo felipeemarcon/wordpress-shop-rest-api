f627d47865e1752fabf75933982acf6f0887b0f5
Felipe Marcon <felipe.marcon@gamersclub.com.br>
2019-09-16 09:21:17 -0300

    Transaction get

--- endpoints/Transaction.php
+++ endpoints/Transaction.php
@@ -1,5 +1,4 @@
 <?php
-
   function createTransaction($request) {
     $currentUser = wp_get_current_user();
     $currentUserId = $currentUser->ID;
@@ -41,6 +40,56 @@
 
   }
 
+  // Get transactions
+  function getTransactions($request) {
+
+    $transactionType = sanitize_text_field($request['type']);
+    $currentUser = wp_get_current_user();
+    $currentUserId = $currentUser->ID;
+
+    if ($currentUserId) {
+    $userEmail - get_userdata($currentUserId)->user_email;
+    $metaQuery = null;
+
+    if ($transactionType) {
+      $metaQuery = array(
+        'key' => $transactionType,
+        'value' => $userEmail,
+        'compair' => '='
+      );
+    }
+
+
+    $query = array(
+      'post_type' => 'transaction',
+      'orderby' => 'date',
+      'posts_per_page' => -1,
+      'meta_query' => array($metaQuery),
+    );
+
+    $loop = new WP_Query($query);
+    $posts = $loop->posts;
+
+    $response = array();
+
+    foreach ($posts as $key => $value) {
+      $postId = $value->ID;
+      $postMeta = get_post_meta($postId);
+
+      $response[] = array(
+        "buyerId" => $postMeta['buyerId'][0],
+        "sellerId" => $postMeta['sellerId'][0],
+        "address" => json_decode($postMeta['address'][0]),
+        "product" => json_decode($postMeta['product'][0]),
+        "date" => $value->post_date,
+      );
+    }
+  } else {
+    $response = new WP_Error('permission', 'User do not have permission.', array('status' => 401));
+  }
+    return rest_ensure_response($response);
+  }
+
   // Registers
   function registerCreateTransaction() {
     register_rest_route('api/v1', '/transaction', array(
@@ -51,6 +100,16 @@
     ));
   }
 
+  function registerGetTransaction() {
+    register_rest_route('api/v1', '/transaction', array(
+      array(
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'getTransactions'
+      ),
+    ));
+  }
+
   // Hooks
   add_action('rest_api_init', 'registerCreateTransaction');
+  add_action('rest_api_init', 'registerGetTransaction');
 ?>
